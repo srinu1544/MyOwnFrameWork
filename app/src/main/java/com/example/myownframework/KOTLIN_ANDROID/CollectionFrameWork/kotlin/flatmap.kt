66a73f80ffc3ba8fa3d9b7543dee512a7805796c
srinu <srinu1544@gmail.com>
2024-03-28 12:54:24 +0530

    updated

---
+++ app/src/main/java/com/example/myownframework/KOTLIN_ANDROID/CollectionFrameWork/kotlin/flatmap.kt
@@ -0,0 +1,14 @@
+package com.example.myownframework.Kotlin_Android.CollectionFrameWork
+
+/*what is flatmap in kotlin ?
+---------------------------
+FlatMap is used to combine all the items of lists into one list */
+
+
+fun main(){
+
+    val numbers = listOf(listOf(1, 2), listOf(3, 4), listOf(5))
+    val flattenedNumbers = numbers.flatMap { it }
+    println(flattenedNumbers) // [1, 2, 3, 4, 5]
+}
+
---
+++ app/src/main/java/com/example/myownframework/KOTLIN_ANDROID/CollectionFrameWork/kotlin/persistantList.kt
@@ -0,0 +1,26 @@
+package com.example.myownframework.Kotlin_Android.CollectionFrameWork
+
+/*
+A persistent list in Kotlin is a list that cannot be modified after it is created.
+This means that you cannot add, remove, or replace elements in the list. However,
+you can create a new list that is a copy of the original list with the changes you want.
+
+Persistent lists are useful because they are immutable. This means that they cannot be changed
+by accident or by another part of your program. This makes them ideal for use in situations where
+ you need to ensure that the data is always consistent.
+
+There are a few different ways to create a persistent list in Kotlin. One way is to use the
+ persistentListOf() function. This function takes a list of elements as its argument and
+ returns a new persistent list that contains those elements.
+
+Another way to create a persistent list is to use the persistentSetOf() function. This function
+ takes a set of elements as its argument and returns a new persistent set that contains those elements.
+
+Finally, you can also create a persistent list using the persistentMapOf() function. This function takes
+ a map of elements as its argument and returns a new persistent map that contains those elements.
+
+Persistent lists are a powerful tool that can be used to make your code more reliable and easier to
+ maintain. If you are working with a situation where you need to ensure that the data is always consistent,
+ then you should consider using a persistent list.*/
+
+
